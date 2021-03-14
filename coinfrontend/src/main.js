539067a7ca88801d0aa87382b7f09beb7b617b76
高金比尔 <44197741+gjbegjbe@users.noreply.github.com>
2021-03-14 17:20:28 +0800

    feat: 初始化知识图谱结构

--- coinfrontend/src/main.js
+++ coinfrontend/src/main.js
@@ -1,15 +1,17 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import Vue from 'vue'
-import App from './App'
-import router from './router'
+import Vue from "vue"
+import App from "./App.vue"
+import router from "./router"
+import store from "./store"
+import ElementUI from "element-ui"
+import "element-ui/lib/theme-chalk/index.css"
+import axios from "axios"
 
 Vue.config.productionTip = false
-
-/* eslint-disable no-new */
+Vue.use(ElementUI)
+//Vue.use(axios);
+Vue.prototype.$axios = axios
 new Vue({
-  el: '#app',
   router,
-  components: { App },
-  template: '<App/>'
-})
+  store,
+  render: (h) => h(App),
+}).$mount("#app")
