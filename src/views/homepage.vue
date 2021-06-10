7850a3c081d603cafc8f618fae555a1d2f4afef1
高金比尔 <44197741+gjbegjbe@users.noreply.github.com>
2021-06-10 17:19:08 +0800

    feat: 恢复了编辑框，增加了详细信息框

--- src/views/homepage.vue
+++ src/views/homepage.vue
@@ -33,23 +33,6 @@ export default {
     return {
       grouplist: ["锦江国际集团", "华住酒店集团"],
       activeName: '亚朵集团',
-      options: [{
-        value: '选项1',
-        label: '黄金糕'
-      }, {
-        value: '选项2',
-        label: '双皮奶'
-      }, {
-        value: '选项3',
-        label: '蚵仔煎'
-      }, {
-        value: '选项4',
-        label: '龙须面'
-      }, {
-        value: '选项5',
-        label: '北京烤鸭'
-      }],
-      value: ''
     };
   },
   mounted() {},
